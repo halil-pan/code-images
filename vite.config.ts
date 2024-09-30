1648261e6cbc770d3897632ce7ad73256cc93b00
Bati <no-reply@batijs.dev>
2024-09-30 14:57:50 +0800

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import vercel from "vite-plugin-vercel";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [
+    vike({
+      prerender: true,
+    }),
+    react({}),
+    vercel(),
+  ],
+
+  resolve: {
+    alias: {
+      "@": new URL("./", import.meta.url).pathname,
+    },
+  },
+});
